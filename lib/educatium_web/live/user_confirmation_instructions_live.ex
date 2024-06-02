031952af1e766eb2ddc64104565ea8063f4607b4
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2024-06-02 15:00:41 +0100

    feat: add all missing translations

--- lib/educatium_web/live/user_confirmation_instructions_live.ex
+++ lib/educatium_web/live/user_confirmation_instructions_live.ex
@@ -7,22 +7,22 @@ defmodule EducatiumWeb.UserConfirmationInstructionsLive do
     ~H"""
     <div class="mx-auto max-w-sm">
       <.header class="text-center">
-        No confirmation instructions received?
-        <:subtitle>We'll send a new confirmation link to your inbox</:subtitle>
+        <%= gettext("No confirmation instructions received?") %>
+        <:subtitle><%= gettext("We'll send a new confirmation link to your inbox") %></:subtitle>
       </.header>
 
       <.simple_form for={@form} id="resend_confirmation_form" phx-submit="send_instructions">
         <.input field={@form[:email]} type="email" placeholder="Email" required />
         <:actions>
           <.button phx-disable-with="Sending..." class="w-full">
-            Resend confirmation instructions
+          <%= gettext("Resend confirmation instructions") %>
           </.button>
         </:actions>
       </.simple_form>
 
       <p class="text-center mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
+        <.link href={~p"/users/register"}><%= gettext("Register") %></.link>
+        | <.link href={~p"/users/log_in"}><%= gettext("Log in") %></.link>
       </p>
     </div>
     """
@@ -41,7 +41,7 @@ defmodule EducatiumWeb.UserConfirmationInstructionsLive do
     end
 
     info =
-      "If your email is in our system and it has not been confirmed yet, you will receive an email with instructions shortly."
+      gettext("If your email is in our system and it has not been confirmed yet, you will receive an email with instructions shortly.")
 
     {:noreply,
      socket
--- lib/educatium_web/live/user_reset_password_live.ex
+++ lib/educatium_web/live/user_reset_password_live.ex
@@ -6,7 +6,7 @@ defmodule EducatiumWeb.UserResetPasswordLive do
   def render(assigns) do
     ~H"""
     <div class="mx-auto max-w-sm">
-      <.header class="text-center">Reset Password</.header>
+      <.header class="text-center"><%= gettext("Reset Password") %></.header>
 
       <.simple_form
         for={@form}
@@ -15,24 +15,24 @@ defmodule EducatiumWeb.UserResetPasswordLive do
         phx-change="validate"
       >
         <.error :if={@form.errors != []}>
-          Oops, something went wrong! Please check the errors below.
+          <%= gettext("Oops, something went wrong! Please check the errors below.") %>
         </.error>
 
-        <.input field={@form[:password]} type="password" label="New password" required />
+        <.input field={@form[:password]} type="password" label={gettext("New password")} required />
         <.input
           field={@form[:password_confirmation]}
           type="password"
-          label="Confirm new password"
+          label={gettext("Confirm new password")}
           required
         />
         <:actions>
-          <.button phx-disable-with="Resetting..." class="w-full">Reset Password</.button>
+          <.button phx-disable-with={gettext("Resetting...")} class="w-full"><%= gettext("Reset Password") %></.button>
         </:actions>
       </.simple_form>
 
       <p class="text-center text-sm mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
+        <.link href={~p"/users/register"}><%= gettext("Register") %></.link>
+        | <.link href={~p"/users/log_in"}><%= gettext("Log in") %></.link>
       </p>
     </div>
     """
@@ -60,7 +60,7 @@ defmodule EducatiumWeb.UserResetPasswordLive do
       {:ok, _} ->
         {:noreply,
          socket
-         |> put_flash(:info, "Password reset successfully.")
+         |> put_flash(:info, gettext("Password reset successfully."))
          |> redirect(to: ~p"/users/log_in")}
 
       {:error, changeset} ->
@@ -78,7 +78,7 @@ defmodule EducatiumWeb.UserResetPasswordLive do
       assign(socket, user: user, token: token)
     else
       socket
-      |> put_flash(:error, "Reset password link is invalid or it has expired.")
+      |> put_flash(:error, gettext("Reset password link is invalid or it has expired."))
       |> redirect(to: ~p"/")
     end
   end
