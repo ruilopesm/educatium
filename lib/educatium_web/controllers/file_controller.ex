42d293aa0cdf687acdaf03101e1afeaa7100d64e
daniel_sp <mail@danielpereira.xyz>
2024-06-13 18:44:19 +0100

    fix: file download

--- lib/educatium_web/controllers/file_controller.ex
+++ lib/educatium_web/controllers/file_controller.ex
@@ -6,10 +6,9 @@ defmodule EducatiumWeb.FileController do
 
   def download_file(conn, %{"file_id" => file_id}) do
     file = Resources.get_file!(file_id)
-    file_url= File.url({file.file, file}, :original) |> IO.inspect(label: "URL")
+    file_path = File.url({file.file, file}, :original)
 
     conn
-    |> put_resp_header("content-disposition", "attachment; filename=\"#{file.name}\"")
-    |> send_download({:file, file_url})
+    |> redirect(to: file_path)
   end
 end
