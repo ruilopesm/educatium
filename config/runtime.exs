df31f22b8e5cc6c3f2e27744cdec882408193ca1
Rui Lopes <ruilopesm19@gmail.com>
2024-06-15 00:34:06 +0100

    style: format

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,6 +17,7 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 import Dotenvy
+
 source([".env", ".env.#{config_env()}", ".env.#{config_env()}.local"])
 
 if System.get_env("PHX_SERVER") do
