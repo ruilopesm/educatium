a5a642b07c696833fb1d172b0b2151676c93206f
Rui Lopes <ruilopesm19@gmail.com>
2024-06-15 23:04:15 +0100

    fix: remove style and lint actions

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
-  plugins: [DoctestFormatter, Phoenix.LiveView.HTMLFormatter, Styler, TailwindFormatter],
+  plugins: [DoctestFormatter, Phoenix.LiveView.HTMLFormatter, TailwindFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
 ]
--- test/educatium_web/live/user_login_live_test.exs
+++ test/educatium_web/live/user_login_live_test.exs
@@ -31,7 +31,8 @@ defmodule EducatiumWeb.UserLoginLiveTest do
 
       {:ok, lv, _html} = live(conn, ~p"/users/log_in")
 
-      form = form(lv, "#login_form", user: %{email: user.email, password: password, remember_me: true})
+      form =
+        form(lv, "#login_form", user: %{email: user.email, password: password, remember_me: true})
 
       conn = submit_form(form, conn)
 
@@ -43,7 +44,10 @@ defmodule EducatiumWeb.UserLoginLiveTest do
     } do
       {:ok, lv, _html} = live(conn, ~p"/users/log_in")
 
-      form = form(lv, "#login_form", user: %{email: "test@email.com", password: "123456", remember_me: true})
+      form =
+        form(lv, "#login_form",
+          user: %{email: "test@email.com", password: "123456", remember_me: true}
+        )
 
       conn = submit_form(form, conn)
 
